482b89a083043884ebf8f775b7ceec3e2d78f7c7
Yevhen Vynohradov <ve.geodes@gmail.com>
2022-11-08 01:05:03 +0200

    complete 03

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -38,7 +38,7 @@ const fp = flatpickr(refs.dateTimeInput, options);
 function checkingDate (){
   currenTime < dateSelected ? 
   refs.startBtn.removeAttribute('disabled') :
-  alert('Please choose the date that in the future')
+  alert('Please choose the date in the future')
 }
 
 function handleStartBtn (event) {
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+const formContainer = document.querySelector('.form')
+const refs = {
+  inputDelay: formContainer.querySelector('[name = delay]'),
+  inputStep: formContainer.querySelector('[name = step]'),
+  inputAmount: formContainer.querySelector('[name = amount]'),
+  btnSub: formContainer.querySelector('[type= submit]'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  })
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }).catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  })
+}
+
+function handleBtnSub (event) {
+  event.preventDefault();
+  startPromiseCycle(getInputValues(),createPromise)
+}
+
+function startPromiseCycle(obj,fn){
+  let currentDelay = obj.delay;
+  for (let i = 1; i <= obj.amount; i+=1) {
+    fn(i,currentDelay);
+    currentDelay = currentDelay + obj.step
   }
 }
+
+function getInputValues () {
+return {
+    delay: Number(refs.inputDelay.value),
+    step: Number(refs.inputStep.value), 
+    amount: refs.inputAmount.value,
+    // test: 'test',
+  }
+}
+
+refs.btnSub.addEventListener('click', handleBtnSub);
